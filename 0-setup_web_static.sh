7e3ab2657a699e57185609150e863f8e02721f0f
Anoop M <noop2016@gmail.com>
2017-11-21 18:49:27 +0000

    added comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-#create new dir and file to host hbnb_static
-#changes ownership to ubuntu
-#adds location element to /hbnb_static
+# Setup new location block for `/hbnb_static`
 
+# create directories
 mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
 
+# add test string 
 echo "Hello New file" > /data/web_static/releases/test/index.html
 
+# create a soft link with test -> current
 ln -fs /data/web_static/releases/test /data/web_static/current 
 
+# give full ownership of all files in data dir
 chown -R ubuntu:ubuntu /data
 
+# add new location to URI to point to the softlink current
 sed -i '/.*internal.*/a \\t\tlocation /hbnb_static { alias /data/web_static/current; }' \
 	/etc/nginx/sites-available/holberton
 
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,19 +6,20 @@ from datetime import datetime
 import os
 
 env.user = 'ubuntu'
-env.hosts = ['142.44.167.236', '144.217.246.202']
-env.key_filename = '~/.ssh/holberton'
+env.hosts = ['142.44.167.236', '144.217.246.202']  # run on both servers
+env.key_filename = '~/.ssh/holberton'  # path to my ssh public key
 
 
 def do_pack():
     """Compress all `web_static` files"""
 
-    now = datetime.now()
+    now = datetime.now()  # get current date and time
     name = "web_static_" + now.strftime('%Y%m%d%H%M')
 
-    local("mkdir -p versions")
+    local("mkdir -p versions")  # create dir
 
-    with lcd("versions"):
+    with lcd("versions"):  # inside the version dir
+        # compress web_static to .tgz -> archive
         result = local("tar -cvzf {}.tgz ../web_static"
                        .format(name), capture=True)
         print(result)
@@ -26,36 +27,37 @@ def do_pack():
         path = local("pwd", capture=True)
         result = "{:s}/{:s}.tgz".format(path, name) if result.succeeded else None
 
-    return result
+    return result  # return the path to archive if successful else None
 
 
 def do_deploy(archive_path):
     """deploy the web_static"""
 
+    # check if archive exist
     if not os.path.isfile(archive_path):
         return False
 
-    archive = os.path.basename(archive_path)
-    dirname = "/data/web_static/releases/" + archive[:-4]
+    archive = os.path.basename(archive_path)  # get only the archive name
+    dirname = "/data/web_static/releases/" + archive[:-4]  # dir path
 
     try:
-        put(archive_path, "/tmp", use_sudo=True)
+        put(archive_path, "/tmp", use_sudo=True)  # transfer file
 
-        run("mkdir -p " + dirname)
+        run("mkdir -p " + dirname)  # create dir
 
         with cd("/tmp"):
-            run("tar -xzvf {}".format(archive))
-            run("mv web_static/* {}/".format(dirname))
-            run("rm -rf {} web_static".format(archive))
+            run("tar -xzvf {}".format(archive))  # uncompress archive
+            run("mv web_static/* {}/".format(dirname))  # movie contents
+            run("rm -rf {} web_static".format(archive))  # clear archive
 
-        run("rm /data/web_static/current")
-        run("ln -fs {} /data/web_static/current".format(dirname))
+        run("rm /data/web_static/current")  # remove softlink
+        run("ln -fs {} /data/web_static/current".format(dirname))  # new link
 
-        print(run("sudo service nginx restart"))
-    except:
+        print(run("sudo service nginx restart"))  # restart nginx
+    except:  # if any of the commands above failed return False
         return False
 
-    return True
+    return True  # everything ran successfuly
 
 
 def deploy():
